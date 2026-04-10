8afbbe15f2b73b69b611c38d75b5dd6d720ef782
Daniel Bernal <jose.bernalm@upb.edu.co>
2026-04-10 17:20:02 -0500

    Add configuration for app and Firebase

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const APP = {
+  albumId: "main",              // single album
+  albumTitle: "Our Album",      // change this
+  // IMPORTANT: set this AFTER you run the admin init step (see README)
+  // It will be created in Firestore albums/main
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYvgxaUdfFznw9wQ6xNLMgXnVHqZ4Ywlo",
+  authDomain: "couples-album.firebaseapp.com",
+  projectId: "couples-album",
+  storageBucket: "couples-album.firebasestorage.app",
+  messagingSenderId: "824100065719",
+  appId: "1:824100065719:web:bd5526b4766ab7c2229832"
+};
